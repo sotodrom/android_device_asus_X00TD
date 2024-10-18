77e9a908489e15bf3d717c9c33491a8aaf3920e4
Tiktodz <dotkit@electrowizard.me>
2024-10-18 16:59:30 +0000

    X00TD: Matrixx bringup
    - Also regenerate
    
    Change-Id: Ibfaa591aac7d8fa7d26d1d3119b67a8eda0d77e2

---
+++ vendorsetup.sh
@@ -0,0 +1,27 @@
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/userspace
+rm -rf kernel/asus/sdm660/KernelSU/website
+rm -rf kernel/asus/sdm660/KernelSU/scripts
+rm -rf kernel/asus/sdm660/KernelSU/docs
+
+rm -rf device/asus/sdm660-common
+#rm -rf device/asus/X00TD
+git clone --depth=1 https://github.com/sotodrom/android_device_asus_sdm660-common -b master device/asus/sdm660-common
+#git clone --depth=1 https://github.com/sotodrom/android_device_asus_X00TD -b master device/asus/X00TD
+
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/sotodrom/proprietary_vendor_asus -b master vendor/asus
+
+rm -rf vendor/lineage-priv/keys
+git clone https://github.com/sotodrom/keys vendor/lineage-priv/keys
+
+cd packages/apps/FMRadio
+git revert 3a33db46414421a81e75cc4af07bc9b4eba528f2 --no-edit
+cd ../../..
+cd vendor/qcom/opensource/libfmjni
+git revert 055b0bed7a5f8822fc7c4658fff44c886177cb9b --no-edit
+cd ../../../..
+
+export KBUILD_BUILD_USER=queen
+export TZ=Asia/Jakarta
