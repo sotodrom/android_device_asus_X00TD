cc73eb890763e6f4cac3f99cba485d88af43c2df
Meisie Widuri <dotkit@electrowizard.me>
2024-11-12 11:31:46 +0700

    X00TD: Declare overlay-lineage for touchgesture

--- device.mk
+++ device.mk
@@ -20,7 +20,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Powerhint
 PRODUCT_COPY_FILES += \
