8017588a0f3d7ca16b830d46fba90b012bbc8ae2
Meisie Widuri <dotkit@electrowizard.me>
2024-12-04 02:04:23 +0700

    X00TD: Update and rename lineage_X00TD.mk to aosp_X00TD.mk

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -10,30 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_BUILD_DEVICE_AS_WEBCAM := false
-TARGET_ENABLE_BLUR := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Matrixx
-MATRIXX_MAINTAINER := ∂σткιт
-WITH_GMS := false
-
-# Extra Matrixx Stuff
-MATRIXX_CHIPSET := SDM660
-MATRIXX_BATTERY := 5000mAh
-MATRIXX_DISPLAY := 1080x2160
-BUILD_GOOGLE_CONTACTS := false
-BUILD_GOOGLE_DIALER := false
-BUILD_GOOGLE_MESSAGE := false
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := aosp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
