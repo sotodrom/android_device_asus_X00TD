99203051f0a62127a94ef1db5244dd72dcf188f5
Meisie Widuri <dotkit@electrowizard.me>
2024-12-04 02:10:07 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/texascake/android_kernel_asus_sdm660-4.19 -b test kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU/userspace
 rm -rf kernel/asus/sdm660/KernelSU/website
 rm -rf kernel/asus/sdm660/KernelSU/scripts
@@ -14,7 +14,7 @@ rm -rf vendor/asus
 git clone --depth=1 https://github.com/sotodrom/proprietary_vendor_asus -b master vendor/asus
 
 rm -rf vendor/lineage/signing/keys
-git clone https://github.com/sotodrom/keys -b evos vendor/lineage/signing/keys
+git clone https://github.com/sotodrom/keys -b evos signature && cp -R signature/* vendor/aosp/signing/keys/
 
 export KBUILD_BUILD_USER=queen
 export TZ=Asia/Jakarta
