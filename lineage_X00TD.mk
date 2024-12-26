a6601dec89198d2c1d78e39305d5e956326edd21
Kneba <abenkenary3@gmail.com>
2024-12-26 10:27:45 +0700

    X00TD: Update and rename blaze_X00TD.mk to lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -7,28 +7,27 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common ProjectBlaze stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Blaze Official Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := ∂σткιт
-TARGET_BUILD_DEVICE_AS_WEBCAM := false
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_BCR := true
-TARGET_INCLUDE_GCAMGO := true
-WITH_GMS := true
+#BLAZE_BUILD_TYPE := OFFICIAL
+#BLAZE_MAINTAINER := ∂σткιт
+#TARGET_BUILD_DEVICE_AS_WEBCAM := false
+#TARGET_FACE_UNLOCK_SUPPORTED := true
+#TARGET_INCLUDE_BCR := true
+#TARGET_INCLUDE_GCAMGO := true
+#WITH_GMS := true
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := blaze_X00TD
+PRODUCT_NAME := lineage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
