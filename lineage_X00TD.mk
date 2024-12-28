263d921a50d385d174f36d30d63c71a18a939b16
Meisie Widuri <dotkit@electrowizard.me>
2024-12-28 12:02:24 +0700

    X00TD: Update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -36,11 +36,4 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_DEVICE=ASUS_X00TD \
-    PRODUCT_NAME=WW_X00TD
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sdm660_64-user 10 QKQ1 72 release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys"
+    DeviceProduct=ASUS_X00TD
