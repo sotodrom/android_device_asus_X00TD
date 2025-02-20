c092d1c14e0141048644b31c90e17234033b316f
strongreasons <abenkenari@gmail.com>
2025-02-20 12:31:26 +0700

    X00TD: Update and rename lineage_X00TD.mk to flare_X00TD.mk

--- flare_X00TD.mk
+++ flare_X00TD.mk
@@ -9,25 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ProjectBlaze stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/flare/config/common_full_phone.mk)
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Blaze Official Stuff
-#BLAZE_BUILD_TYPE := OFFICIAL
-#BLAZE_MAINTAINER := ∂σткιт
-#TARGET_BUILD_DEVICE_AS_WEBCAM := false
-#TARGET_FACE_UNLOCK_SUPPORTED := true
-#TARGET_INCLUDE_BCR := true
-#TARGET_INCLUDE_GCAMGO := true
-#WITH_GMS := true
+FLARE_MAINTAINER := zkneb
+FLARE_BUILD_TYPE := OFFICIAL
+WITH_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_ENABLE_BLUR := false
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := flare_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -36,4 +34,6 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceProduct=ASUS_X00TD
+    DeviceProduct=ASUS_X00TD \
+    BuildDesc="sdm660_64-user 10 QKQ1 72 release-keys" \
+    BuildFingerprint=asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
