68350fab37035aba146c46e375bd13646561b7b0
strongreasons <abenkenari@gmail.com>
2025-02-20 12:33:15 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
-rm -rf kernel/asus/sdm660/KernelSU/userspace
-rm -rf kernel/asus/sdm660/KernelSU/website
-rm -rf kernel/asus/sdm660/KernelSU/scripts
-rm -rf kernel/asus/sdm660/KernelSU/docs
+rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
+rm -rf kernel/asus/sdm660/KernelSU-Next/website
+rm -rf kernel/asus/sdm660/KernelSU-Next/scripts
+rm -rf kernel/asus/sdm660/KernelSU-Next/docs
 
 rm -rf device/asus/sdm660-common
 #rm -rf device/asus/X00TD
@@ -13,8 +13,8 @@ git clone --depth=1 https://github.com/sotodrom/android_device_asus_sdm660-commo
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/sotodrom/proprietary_vendor_asus -b 15 vendor/asus
 
-rm -rf vendor/lineage/signing/keys
-git clone https://github.com/sotodrom/keys -b evos vendor/lineage/signing/keys
+#rm -rf vendor/lineage/signing/keys
+#git clone https://github.com/sotodrom/keys -b evos vendor/lineage/signing/keys
 
 export KBUILD_BUILD_USER=queen
 export TZ=Asia/Jakarta
