eb0788f73b44836fbd255f84e4c2f6d6d29f1693
Meisie Widuri <dotkit@electrowizard.me>
2025-02-23 22:33:31 +0700

    Update and rename infinity_X00TD.mk to lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -9,16 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ProjectBlaze stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-TARGET_SUPPORTS_CALL_RECORDING := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-INFINITY_BUILD_TYPE := UNOFFICIAL
-TARGET_SUPPORTS_BLUR := false
-WITH_GAPPS := false
-TARGET_SHIPS_FULL_GAPPS := false
-TARGET_BUILD_GOOGLE_TELEPHONY := false
-INFINITY_MAINTAINER := qǝuʞz
+TARGET_DISABLE_EPPE := true
+RISING_MAINTAINER := zkneb
+WITH_GMS := false
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -27,7 +23,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := infinity_X00TD
+PRODUCT_NAME := lineage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
@@ -36,4 +32,5 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceProduct=ASUS_X00TD
+    DeviceProduct=ASUS_X00TD \
+    RisingChipset="Snapdragon 636"
