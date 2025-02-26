f494c4935fff7bf210642ecb9f9309c6a1e17dfd
texascake <budalblimbing@gmail.com>
2025-02-26 21:31:22 +0700

    Update and rename derp_X00TD.mk to voltage_X00TD.mk

--- voltage_X00TD.mk
+++ voltage_X00TD.mk
@@ -9,13 +9,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common ProjectBlaze stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
 TARGET_DISABLE_EPPE := true
-TARGET_SUPPORTS_OMX_SERVICE := false
-USE_LEGACY_BOOTANIMATION := true
-TARGET_NOT_USES_BLUR := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -24,7 +21,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := derp_X00TD
+PRODUCT_NAME := voltage_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
