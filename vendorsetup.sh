f425dd52ffe5c3d864864794c0494c45e52c2a82
Kneba <abenkenary3@gmail.com>
2025-03-24 12:01:49 +0700

    X00TD: Update kerneltree

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b master kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b wip kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 rm -rf kernel/asus/sdm660/KernelSU-Next/website
 rm -rf kernel/asus/sdm660/KernelSU-Next/scripts
@@ -8,7 +8,7 @@ rm -rf kernel/asus/sdm660/KernelSU-Next/docs
 rm -rf device/asus/sdm660-common
 #rm -rf device/asus/X00TD
 git clone --depth=1 https://github.com/sotodrom/android_device_asus_sdm660-common -b 15 device/asus/sdm660-common
-#git clone --depth=1 https://github.com/sotodrom/android_device_asus_X00TD -b master device/asus/X00TD
+#git clone --depth=1 https://github.com/sotodrom/android_device_asus_X00TD -b infinity device/asus/X00TD
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/sotodrom/proprietary_vendor_asus -b 15 vendor/asus
