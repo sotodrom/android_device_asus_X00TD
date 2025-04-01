9506187f5922a4507fe9c1a2c012be4bd0943797
Kneba <abenkenary3@gmail.com>
2025-04-01 23:00:10 +0700

    X00TD: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,16 @@
 rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b wip kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
-rm -rf kernel/asus/sdm660/KernelSU-Next/website
-rm -rf kernel/asus/sdm660/KernelSU-Next/scripts
-rm -rf kernel/asus/sdm660/KernelSU-Next/docs
 
 rm -rf device/asus/sdm660-common
 #rm -rf device/asus/X00TD
 git clone --depth=1 https://github.com/sotodrom/android_device_asus_sdm660-common -b 15 device/asus/sdm660-common
-#git clone --depth=1 https://github.com/sotodrom/android_device_asus_X00TD -b infinity device/asus/X00TD
+#git clone --depth=1 https://github.com/sotodrom/android_device_asus_X00TD -b pixelos device/asus/X00TD
 
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/sotodrom/proprietary_vendor_asus -b 15 vendor/asus
 
-rm -rf vendor/lineage-priv/keys
-rm -rf vendor/infinity/signing/keys
-mkdir -p vendor/lineage-priv/keys
-git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
+git clone https://github.com/electrolaboratory/public-keys vendor/aosp/signing/keys/
 
 export KBUILD_BUILD_USER=queen
 export BUILD_USER=queen
