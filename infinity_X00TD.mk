f3e3c855666a64777bbab8f127ce7fcb2cc76200
Meisie Widuri <dotkit@electrowizard.me>
2025-04-02 18:57:22 +0700

    X00TD: Update infinity_X00TD.mk

--- infinity_X00TD.mk
+++ infinity_X00TD.mk
@@ -14,14 +14,14 @@ $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
 
-TARGET_FACE_UNLOCK_SUPPORTED := true
 INFINITY_BUILD_TYPE := UNOFFICIAL
 INFINITY_MAINTAINER := qǝuʞz
+TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_BLUR := false
 
 # Gapps variant
-TARGET_SHIPS_FULL_GAPPS := false
 TARGET_BUILD_GOOGLE_TELEPHONY := false
+TARGET_SHIPS_FULL_GAPPS := false
 TARGET_SUPPORTS_CALL_RECORDING := false
 WITH_GAPPS := false
 
@@ -38,4 +38,6 @@ PRODUCT_MANUFACTURER := asus
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DeviceProduct=ASUS_X00TD
+    DeviceProduct=ASUS_X00TD \
+    BuildDesc="sdm660_64-user 10 QKQ1 72 release-keys" \
+    BuildFingerprint=asus/ASUS_X00TD/ASUS_X00TD:10/QKQ1/17.2017.2012.438-20201203:user/release-keys
