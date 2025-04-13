29b25cc72534feeb902b52d134f979b493e449d9
Kneba <abenkenary3@gmail.com>
2025-04-13 14:13:17 +0700

    X00TD: Update kernel and vibrator

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 rm -rf kernel/asus/sdm660
-git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b stable-release kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b stable kernel/asus/sdm660
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
 rm -rf device/asus/sdm660-common
@@ -10,12 +10,14 @@ git clone --depth=1 https://github.com/texascake/android_device_asus_sdm660-comm
 rm -rf vendor/asus
 git clone --depth=1 https://github.com/sotodrom/proprietary_vendor_asus -b 15 vendor/asus
 
+rm -rf vendor/qcom/opensource/vibrator
+git clone --depth=1 https://github.com/electrolaboratory/android_vendor_qcom_opensource_vibrator -b lineage-22.2 vendor/qcom/opensource/vibrator
+
 rm -rf vendor/lineage-priv/keys
 rm -rf vendor/infinity/signing/keys
 mkdir -p vendor/lineage-priv/keys
 git clone https://github.com/electrolaboratory/public-keys vendor/lineage-priv/keys/
 
-export KBUILD_BUILD_USER=queen
 export BUILD_USER=queen
 export BUILD_USERNAME=queen
 export TZ=Asia/Jakarta
